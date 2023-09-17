ffb40891969354c56a5c1ed91365aa2a4088640d
bushramahin <101120359+bushramahin@users.noreply.github.com>
2023-09-17 17:12:54 +0500

    Initial commit

---
+++ src/functions.ts
@@ -0,0 +1,4 @@
+export function random (){
+    let num = Math.random()
+   return  num.toFixed(2)
+}
---
+++ src/game.ts
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+import showBanner from "node-banner";
+import { random } from "./functions.js";
+import gradient from "gradient-string";
+let guess = [
+    {
+        name : "gussed_number",
+        type : "number",
+        message : gradient.passion("What do you think which number is this?") 
+    }
+];
+let again_guess = [
+    {
+        name : "again",
+        type : "confirm",
+        message :gradient.pastel("Do you want to guess again?")
+    }
+]
+
+
+
+async function thinking (){
+    let condition = true
+   while(condition){
+    let {gussed_number} = await inquirer.prompt(guess);
+    let correct_num = random()
+        if(correct_num == gussed_number){
+            // (async ()=>{
+            //     await showBanner('You Won' , 'green')
+            // })();
+            console.log("You Won");
+            console.log("The number is : " , random());
+            
+        }
+       else{
+        // (async () => {
+        //     await showBanner('Unfortunately You Lose !')
+        // })();
+        console.log("Unfortunately You Lose !");
+        console.log("The number is : " , random());
+        }
+        let {again} = await inquirer.prompt(again_guess)
+        condition = again
+   }
+}
+thinking()
